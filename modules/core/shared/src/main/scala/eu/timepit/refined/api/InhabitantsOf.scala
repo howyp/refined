c68b87633a1fb1198a5ecabb7c237f6bbafe4dd5
Howy Perrin <howyp@users.noreply.github.com>
2017-12-21 17:26:34 +0000

    Add Min case class

--- modules/core/shared/src/main/scala/eu/timepit/refined/api/InhabitantsOf.scala
+++ modules/core/shared/src/main/scala/eu/timepit/refined/api/InhabitantsOf.scala
@@ -1,13 +1,14 @@
 package eu.timepit.refined.api
 
 trait InhabitantsOf[FTP] {
-
   def all: Stream[FTP]
-
 }
-
 object InhabitantsOf {
-
   def apply[FTP](implicit ev: InhabitantsOf[FTP]): InhabitantsOf[FTP] = ev
-
+}
+trait Min[FTP] {
+  def min: FTP
+}
+object Min {
+  def apply[FTP](implicit ev: Min[FTP]): Min[FTP] = ev
 }
